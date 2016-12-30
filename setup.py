a16fb1ee9fea0f9aa4195ae08d4738a8a3057371
Peter Wensel <pwensel@gmail.com>
2016-12-30 13:14:26 -0600

    clean up pep8 standards and add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='glicko2',
+      description='Python implementation of glicko2',
+      author='Alexander Nordlund',
+      author_email='deep alexander at gmail com',
+      packages=find_packages(),
+      url='https://github.com/PeterCat12/glicko2',
+      version='1.0.0',
+      keywords=['glicko2', 'python'],
+      install_requires=[
+      ]
+      )
